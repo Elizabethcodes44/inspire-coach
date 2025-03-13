8eb82292ff64be88ae936dd10edf59ff940785ce
Amy Weitzman <amyweitzman@yahoo.com>
2025-03-13 19:32:26 -0400

    add navbar

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
+import { ROUTES, THEME_OPTIONS } from './global';
+import useLocalStorage from 'use-local-storage';
 
 function Home() {
-  return <h2>Home</h2>;
+  return <h2>Home Page</h2>;
+}
+
+function ManageTasks() {
+  return <h2>Manage Tasks Page</h2>;
 }
 
 function App() {
+  const defaultDark = window.matchMedia(`(prefers-color-scheme: ${THEME_OPTIONS.DARK})`).matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? THEME_OPTIONS.DARK : THEME_OPTIONS.LIGHT);
+
   return (
     <Router>
-      <div className="App">
-        <NavBar />
+      <div className="App" data-theme={theme}>
+        <NavBar defaultTheme={theme} onThemeChange={(newTheme) => setTheme(newTheme)} />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path={ROUTES.HOME_PAGE.route} element={<Home />} />
+          <Route path={ROUTES.MANAGE_TASKS.route} element={<ManageTasks />} />
         </Routes>
       </div>
     </Router>
