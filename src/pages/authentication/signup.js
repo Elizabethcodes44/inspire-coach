2c716c47e8f9af03307172e4cebe9810f50198e9
caryourdaysbm <adeyemioluwakayode73@gmail.com>
2025-03-15 09:55:07 +0100

    fixed my issues

--- src/pages/authentication/signup.js
+++ src/pages/authentication/signup.js
@@ -4,11 +4,20 @@ import Footer from '../../components/Footer';
 import Breadcrumb from '../../components/Breadcrumb';
 import SignupMain from './SignupMain';
 import ScrollToTop from '../../components/ScrollTop';
+import { ROUTES, THEME_OPTIONS } from "../../global";
+import useLocalStorage from "use-local-storage";
 
 import Logo from '../../assets/images/logos/inspire-coach-logo.png';
 
 
 const Signup = () => {
+    const defaultDark = window.matchMedia(
+        `(prefers-color-scheme: ${THEME_OPTIONS.DARK})`
+      ).matches;
+      const [theme, setTheme] = useLocalStorage(
+        "theme",
+        defaultDark ? THEME_OPTIONS.DARK : THEME_OPTIONS.LIGHT
+      );
     return (
         <>
             <Header
@@ -16,6 +25,8 @@ const Signup = () => {
                 menuCategoryEnable='enable'
                 headerNormalLogo={Logo}
                 headerStickyLogo={Logo}
+                onThemeChange={(newTheme) => setTheme(newTheme)}
+
             />
 
             <div class="react-wrapper">
