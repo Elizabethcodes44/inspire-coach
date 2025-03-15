39341e02a82ae795cd5d5c5e8951006c04b4cd88
Amy Weitzman <amyweitzman@yahoo.com>
2025-03-15 09:50:43 -0400

    fix styling issues

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -6,7 +6,7 @@ function ProgressBar({ value }) {
   return (
     <div className='progress-bar-container'>
         <span className='progress-bar-label'>Completion:</span>
-        <progress className='progress-bar' max={100} value={value} />
+        <progress className='progress-bar-scale' max={100} value={value} />
         <span className='progress-bar-value-text'>{value}%</span>
     </div>
   );
